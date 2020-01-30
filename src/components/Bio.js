e548dc3d8ba95696934ab63786967299c0f52ef9
enclairfarron <burcelu1018@gmail.com>
2020-01-30 20:44:06 +0800

    first commit

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,7 +13,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Dan Abramov`}
+          alt={`织部里沙`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
